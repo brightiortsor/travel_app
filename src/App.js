4bba645b840c1378d061aa8e70153b2ba11a1781
Bright Iortsor <iortsor@gmail.com>
2023-01-03 17:02:19 +0100

    ADDED/ home component was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import Popular from "./components/Popular/Popular";
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Home />
+      <Popular />
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="sec-container container">
+        <div className="home-text">
+          <h1 className="title">Plan Your Trip with Point X!</h1>
+
+          <p className="sub-title">
+            Travel with Point X and get the best deals on your next trip!
+          </p>
+
+          <button className="btn explore-btn">
+            <a href="#">Explore Now</a>
+          </button>
+        </div>
+
+        <div className="home-card grid">
+          <div className="location-div">
+            <label htmlFor="location">Location</label>
+            <input type="text" placeholder="Dream Destination" />
+          </div>
+
+          <div className="distance-div">
+            <label htmlFor="distance">Distance</label>
+            <input type="text" placeholder="16 / Miles" />
+          </div>
+
+          <div className="price-div">
+            <label htmlFor="price">Price</label>
+            <input type="text" placeholder="$250 - $1000" />
+          </div>
+
+          <button className="btn">Search</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
